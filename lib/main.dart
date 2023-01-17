1ecc3aec04bf1646db4c35954ac89ccb326e2f92
craig <craig@oppkey.com>
2023-01-16 17:26:40 -0800

    finished lesson 2 parallax background

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,20 @@ class CrowGame extends FlameGame {
   @override
   void onLoad() async {
     super.onLoad();
-    ParallaxComponent mountainBackground = await loadParallaxComponent(
-        [ParallaxImageData('background.png')],
-        baseVelocity: Vector2(50, 0),
+    ParallaxComponent mountainBackground = await loadParallaxComponent([
+      ParallaxImageData('sky.png'),
+      ParallaxImageData('clouds_bg.png'),
+      ParallaxImageData('glacial_mountains.png'),
+      ParallaxImageData('clouds_mg_1.png'),
+      ParallaxImageData('cloud_lonely.png'),
+    ],
+        baseVelocity: Vector2(10, 0),
         velocityMultiplierDelta: Vector2(1.6, 1.0));
     add(mountainBackground);
+    add(SpriteComponent(
+      position: size / 2,
+      anchor: Anchor.center,
+      sprite: await loadSprite('crow350x400.png', srcSize: Vector2(350, 400)),
+    ));
   }
 }
